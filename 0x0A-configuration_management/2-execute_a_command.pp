28efe2efc37b0dc4628d6daa397273e0f6d2f8f1
Mesele <getmesy@gmail.com>
2023-03-24 16:50:48 +0000

    correct 2-exexute_a_comand

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,8 @@
 # Using puppet create a manifest that kilss a process
 
+Exec { path => '/usr/bin:/bin' }
+
 exec { 'killmenow':
-  command => 'pkill killmenow',
-  onlyif  => 'pgrep killmenow',
+  command => '/usr/bin/pkill killmenow',
+  onlyif  => '/usr/bin/pgrep killmenow',
 }
