e5b670513ba612040afcfcdf3715b1cfb9934dc0
Mesele <getmesy@gmail.com>
2023-03-27 16:59:54 +0000

    correct 100-puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,11 @@
 # using Puppet to make changes to our configuration file
-file { '/home/ubuntu/.ssh/config':
+file { '/home/vagrant/.ssh/config':
     ensure  => 'present',
-    owner   => 'ubuntu',
-    group   => 'adm',
+    owner   => 'vagrant',
+    group   => 'vagrant',
     mode    => '0600',
     content => "Host 54.80.197.65\n
                 IdentityFile ~/.ssh/school\n
                 PreferredAuthentications publickey\n
                 PasswordAuthentication no\n",
 }
-
