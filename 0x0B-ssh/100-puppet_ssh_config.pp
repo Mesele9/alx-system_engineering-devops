2ae6e6c58ce8d8fe8ee388acfa2a05de3eea3719
Mesele <getmesy@gmail.com>
2023-03-27 15:23:16 +0000

    correction

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,9 @@
 # using Puppet to make changes to our configuration file
 file { '/home/ubuntu/.ssh/config':
-    ensure => 'present',
-    owner  => 'ubuntu',
-    group  => 'adm',
-    mode   => '0600',
+    ensure  => 'present',
+    owner   => 'ubuntu',
+    group   => 'adm',
+    mode    => '0600',
     content => "Host 54.80.197.65\n
                 IdentityFile ~/.ssh/school\n
                 PreferredAuthentications publickey\n
