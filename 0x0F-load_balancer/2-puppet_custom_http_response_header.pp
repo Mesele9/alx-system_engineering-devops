e0f768bc8951baafb8bbbcb7448415552fdaa75b
Mesele <getmesy@gmail.com>
2023-04-09 13:27:32 +0000

    push advanced task

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,15 @@
+# install nginx
+
+package { 'nginx':
+  ensure => installed,
+}
+
+file_line { 'http_header':
+  path   => '/etc/nginx/nginx.conf',
+  match  => 'http {',
+  line   => 'http {\n\ttadd_header X-Served-By \"${hostname}\";',
+}
+
+exec {'run':
+  command => '/usr/sbin/service nginx restart',
+}
