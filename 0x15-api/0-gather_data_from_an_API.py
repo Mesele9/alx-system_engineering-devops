6955281a8532dad551a7a452052c2c9d7b481dfa
Mesele <getmesy@gmail.com>
2023-04-25 03:13:55 +0000

    correct formating

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -25,4 +25,4 @@ if __name__ == '__main__':
           format(emp_name, completed_task, no_tasks))
 
     for lists in completed_lists:
-        print("     {}".format(lists["title"]))
+        print("\t" "{}".format(lists["title"]))
