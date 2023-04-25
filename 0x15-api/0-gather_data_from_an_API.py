b81980bb699ea0951fda8dd60f81b5a1114ea606
Mesele <getmesy@gmail.com>
2023-04-25 02:52:15 +0000

    push the first task

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""a Python script that, using this REST API, for a given employee ID,
+returns information about his/her TODO list progress.
+"""
+import requests
+import sys
+
+
+emp_id = sys.argv[1]
+user_response = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                             .format(emp_id))
+user = user_response.json()
+emp_name = user['name']
+response = requests.get("https://jsonplaceholder.typicode.com/users/{}/todos"
+                        .format(emp_id))
+todo_list = response.json()
+no_tasks = len(todo_list)
+completed_task = 0
+completed_lists = []
+for task in todo_list:
+    if task["completed"]:
+        completed_task += 1
+        completed_lists.append(task)
+
+print("Employee {} is done with tasks({}/{}):".
+      format(emp_name, completed_task, no_tasks))
+
+for lists in completed_lists:
+    print("\t{}".format(lists["title"]))
